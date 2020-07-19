c38471eabf64c33abfc23ff65f85d7dfda88aabe
Isaias Del Mundo <idelmundo68@gmail.com>
2020-07-19 16:57:38 -0700

    done with api and route

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 // dependencies
-var express = require("express");
+const express = require("express");
+const path = require("path");
+const { response } = require("express");
+const app = express()
 
-//
-var app = express()
-var PORT = process.env.PORT || 3000;
-require("./route/route.js")(app)
+// Config
+const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public")) // look at public folder
+// app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public"))) // look at public folder
+
+require("./route/api.js")(app)
+require("./route/route.js")(app)
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
